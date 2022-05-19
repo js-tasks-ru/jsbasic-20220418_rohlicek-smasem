591c9668feba5579ec38c9866467b1bdea0eac4a
Amin Raiymbek <rohlicek_smasem@yahoo.com>
2022-05-19 22:02:36 +0600

    module 1

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+ 
+  if (str.toLowerCase().indexOf('xxx') >= 0 || str.toLowerCase().indexOf('1xbet') >= 0) {
+    return true;
+  }
+  return false;
+     
+    
 }
+
